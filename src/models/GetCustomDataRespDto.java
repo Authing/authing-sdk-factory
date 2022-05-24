67d8d23e485cc3addac27e27b381206f8fed5278
willin <willin@users.noreply.github.com>
2022-05-24 23:52:06 +0000

    deploy: 608116b6a683296440431bc10903524d286d7225

---
+++ src/models/GetCustomDataRespDto.java
@@ -0,0 +1,59 @@
+package cn.authing.sdk.java.dto;
+
+import java.util.List;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+
+public class GetCustomDataRespDto {
+    /**
+     * 业务状态码，可以通过此状态码判断操作是否成功，200 表示成功。
+     */
+    @JsonProperty("code")
+    private Integer code;
+    /**
+     * 描述信息
+     */
+    @JsonProperty("message")
+    private String message;
+    /**
+     * 细分错误码，可通过此错误码得到具体的错误类型。
+     */
+    @JsonProperty("errorCode")
+    private Integer errorCode;
+    /**
+     * 具体的自定义数据值
+     */
+    @JsonProperty("data")
+    private Object data;
+
+    public Integer getCode() {
+        return code;
+    }
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Integer getErrorCode() {
+        return errorCode;
+    }
+    public void setErrorCode(Integer errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public Object getData() {
+        return data;
+    }
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+
+
+}
