3648d986f08c56f6ac515f361d3e1c83ad99288f
廖长江 <cj@authing.cn>
2022-09-26 11:21:35 +0800

    update java script

--- scripts/generate-java-sdk.js
+++ scripts/generate-java-sdk.js
@@ -6,16 +6,15 @@ fs.mkdirSync('./generated/java/src', { recursive: true });
 
 const main = async () => {
     await generate({
-        input: 'https://console.authing.cn/openapi-json',
+        input: 'http://localhost:3000/openapi-json',
         output: './generated/java/src',
         exportCore: false,
         lang: 'java',
     });
-    // execSync('cp -R generated/java/src/models/* ../authing-java-sdk/src/main/java/dto', { encoding: 'utf-8' });
-    // execSync(
-    //     'cp -R generated/java/src/ManagementClient.java ../authing-java-sdk/src/main/java/client/ManagementClient.java'
-    //     , { encoding: 'utf-8' }
-    // );
+    execSync('cp -R generated/java/src/models/* ../authing-java-sdk/src/main/java/cn/authing/sdk/java/dto/', { encoding: 'utf-8' });
+    execSync(
+        'cp -R generated/java/src/ManagementClient.java ../authing-java-sdk/src/main/java/cn/authing/sdk/java/client/ManagementClient.java', { encoding: 'utf-8' }
+    );
 };
 
 main().then(console.log).catch(console.error);
