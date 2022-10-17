a81c763b24dfdeea494235cf67aecc09f63a9ab2
qidong <qidong@authing.cn>
2022-10-17 19:26:38 +0800

    feat: csharp 优化生成的代码结构

--- scripts/generate-csharp-auth-sdk.js
+++ scripts/generate-csharp-auth-sdk.js
@@ -6,7 +6,7 @@ fs.mkdirSync('./generated/csharp/src', { recursive: true });
 
 const generateAuthenticationClient = async () => {
     await generate({
-        input: 'http://8.142.39.176:3000/auth-openapi-json',
+        input: 'https://console.test2.authing-inc.co/auth-openapi-json',
         output: './generated/csharp/src',
         exportCore: false,
         useOptions: true,
