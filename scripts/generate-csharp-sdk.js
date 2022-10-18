a60666ba4038677f73d550c75487c8b7f1603183
qidong <qidong@authing.cn>
2022-10-18 18:23:57 +0800

    feat: charp template 模板生成样式优化

--- scripts/generate-csharp-sdk.js
+++ scripts/generate-csharp-sdk.js
@@ -6,7 +6,7 @@ fs.mkdirSync('./generated/csharp/src', { recursive: true });
 
 const main = async () => {
     await generate({
-        input: 'http://8.142.39.176:3000/openapi-json',
+        input: 'https://console.test2.authing-inc.co/openapi-json',
         output: './generated/csharp/src',
         exportCore: false,
         useOptions: true,
